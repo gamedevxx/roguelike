df855b93de7965a50f4790d8926c235b47a10b26
AnnaRemi <60964233+AnnaRemi@users.noreply.github.com>
2022-11-23 00:38:20 +0300

    Delete AbstractWeapon.cs

--- Assets/Scripts/Things/AbstractWeapon.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class AbstractWeapon : BaseScriptableObj {
-
-    public virtual void Activate(GameObject target) { }
-}
