d9bede3ee69be3703f307b428cb922fbafeae931
AnnaRemi <60964233+AnnaRemi@users.noreply.github.com>
2022-11-23 00:37:10 +0300

    updated weapon;
    no target in activate/deactivate functions

--- Assets/Scripts/Things/Weapon.cs
+++ Assets/Scripts/Things/Weapon.cs
@@ -7,8 +7,25 @@ public class Weapon : AbstractWeapon
 {
     public float damage;
 
-    public override void Activate(GameObject target)
+    List<GameObject> EnemyList = new List<GameObject>();
+    public Enemy[] Temporary;
+
+    public override void Activate()
     {
-        //target.GetComponent<Enemy>().health -= damage;
+        Temporary = FindObjectsOfType<Enemy>();
+        for (int i = 0; i < Temporary.Length; i++) // Finding all Enemy objects and adding them to list
+        {
+            EnemyList.Add(Temporary[i].gameObject);
+        }
+    }
+
+    public override void Deactivate()
+    {
+        for (int i = 0; i < EnemyList.Count; i++)
+        {
+            EnemyList[i].GetComponent<Enemy>().health -= damage;
+
+            EnemyList.RemoveAt(i);
+        }
     }
 }
