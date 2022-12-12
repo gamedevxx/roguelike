1dba928efa64e55fa6d819cc8527245b6fd82677
Иван Строгальщиков <i.strogalshikov@gmail.com>
2022-12-12 22:16:36 +0300

    Changed lights in the rooms

--- Assets/Scripts/Boss/PhaseController.cs
+++ Assets/Scripts/Boss/PhaseController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PhaseController : MonoBehaviour
 {
     private BasePhase[] _phases;
+    private CreatureBody _creatureBody;
 
     private float time = 10f;
     private float curr_time = 0f;
@@ -13,24 +14,29 @@ public class PhaseController : MonoBehaviour
     void Start()
     {
         _phases = GetComponents<BasePhase>();
+        _creatureBody = GetComponent<CreatureBody>();
         ChangePhase();
     }
     
     void Update()
     {
-        curr_time += Time.deltaTime;
-        if (curr_time > 20)
+        if (_creatureBody.Health <= _creatureBody.maxHealth * (1 - (n_phase + 1) / 3))
         {
-            curr_time = 0;
-
+            ++n_phase;
             ChangePhase();
         }
+        // curr_time += Time.deltaTime;
+        // if (curr_time > 20)
+        // {
+        //     curr_time = 0;
+        //
+        //     ChangePhase();
+        // }
     }
 
     public void ChangePhase()
     {
         ++n_phase;
-        
         if (n_phase > 0)
         {
             _phases[n_phase - 1].EndPhase();
